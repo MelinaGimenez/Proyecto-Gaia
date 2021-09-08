502559d5b708a24f7a072071edef364de6c75459
MelinaGimenez <gimenez.melina@hotmail.com>
2021-09-08 20:17:09 -0300

    listo para iniciar el front

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+let publicPath = path.resolve('public');
+app.use(express.static(publicPath));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
